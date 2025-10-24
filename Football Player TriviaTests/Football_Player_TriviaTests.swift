cce75b542f5f056e7bc3bdafc154d538553730c1
seanwh01 <seanwh01@yahoo.com>
2025-10-24 09:25:34 -0400

    Initial Commit

---
+++ Football Player TriviaTests/Football_Player_TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Football_Player_TriviaTests.swift
+//  Football Player TriviaTests
+//
+//  Created by SEAN WHITE on 10/24/25.
+//
+
+import Testing
+@testable import Football_Player_Trivia
+
+struct Football_Player_TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
